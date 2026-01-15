b0600cfd84b7caa5f3511b2ca05e74c7859dcfd1
Yahiro Nakamoto <yahiro0921@gmail.com>
2026-01-15 23:30:09 +0900

    Change to use context env for Notion token
    Notionトークンの取得方法を変更し、process.envではなく、HandlerRequestのenvを使用するように修正しました。

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import { HandlerRequest } from "../notion-api/types.js";
 
 export const getNotionToken = (c: HandlerRequest) => {
   return (
-    process.env.NOTION_TOKEN ||
+    c.env?.NOTION_TOKEN ||
     (c.req.header("Authorization") || "").split("Bearer ")[1] ||
     undefined
   );
